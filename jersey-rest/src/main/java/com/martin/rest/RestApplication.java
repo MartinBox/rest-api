c76a121b66005679b662f8c7fffa3fa39a693572
MartinBOx <1194688733@qq.com>
2015-12-29 17:23:07 +0800

    1、格式化代码

--- jersey-rest/src/main/java/com/martin/rest/RestApplication.java
+++ jersey-rest/src/main/java/com/martin/rest/RestApplication.java
@@ -1,6 +1,5 @@
 package com.martin.rest;
 
- 
 import org.glassfish.jersey.filter.LoggingFilter;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.moxy.json.MoxyJsonFeature;
@@ -19,15 +18,15 @@ import org.glassfish.jersey.server.ResourceConfig;
  */
 public class RestApplication extends ResourceConfig {
 
-	 public RestApplication() {  
+	public RestApplication() {
 
-	     //服务类所在的包路径  
-	     packages("com.martin.rest.resource");  
-	     //注册 MultiPart
-		 register(MultiPartFeature.class);
-	     //注册 JSON 转换器  
-	     register(MoxyJsonFeature.class);
-	     // 输入请求日志。生产环境可以去掉
-	     register(LoggingFilter.class);
-	    } 
+		//服务类所在的包路径  
+		packages("com.martin.rest.resource");
+		//注册 MultiPart
+		register(MultiPartFeature.class);
+		//注册 JSON 转换器  
+		register(MoxyJsonFeature.class);
+		// 输入请求日志。生产环境可以去掉
+		register(LoggingFilter.class);
+	}
 }
--- jersey-rest/src/main/java/com/martin/rest/resource/MyResource.java
+++ jersey-rest/src/main/java/com/martin/rest/resource/MyResource.java
@@ -24,71 +24,70 @@ import com.martin.rest.bean.MyBean;
 @Path("api")
 public class MyResource {
 
-    /**
-     * 方法处理 HTTP GET 请求。返回的对象以"text/plain"媒体类型
+	/**
+	 * 方法处理 HTTP GET 请求。返回的对象以"text/plain"媒体类型
 	 * 给客户端
-     *
-     * @return String 以 text/plain 形式响应
-     */
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Got it!";
-    }
-    
+	 *
+	 * @return String 以 text/plain 形式响应
+	 */
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getIt() {
+		return "Got it!";
+	}
 
-    /**
-     * 方法处理 HTTP GET 请求。返回的对象以"application/xml"媒体类型
+	/**
+	 * 方法处理 HTTP GET 请求。返回的对象以"application/xml"媒体类型
 	 * 给客户端
-     *
-     * @return MyPojo 以 application/xml 形式响应
-     */
-    @GET
-    @Path("pojoxml")
-    @Produces(MediaType.APPLICATION_XML)
-    public MyBean getPojoXml() {
-    	MyBean pojo = new MyBean();
-    	pojo.setName("waylau.com");
-    	pojo.setAge(28);
-        return pojo;
-    }
-    
-    /**
-     * 方法处理 HTTP GET 请求。返回的对象以"application/json"媒体类型
+	 *
+	 * @return MyPojo 以 application/xml 形式响应
+	 */
+	@GET
+	@Path("pojoxml")
+	@Produces(MediaType.APPLICATION_XML)
+	public MyBean getPojoXml() {
+		MyBean pojo = new MyBean();
+		pojo.setName("waylau.com");
+		pojo.setAge(28);
+		return pojo;
+	}
+
+	/**
+	 * 方法处理 HTTP GET 请求。返回的对象以"application/json"媒体类型
 	 * 给客户端
-     *
-     * @return MyPojo 以 application/json 形式响应
-     */
-    @GET
-    @Path("pojojson")
-    @Produces(MediaType.APPLICATION_JSON)
-    public MyBean getPojoJson() {
-    	MyBean pojo = new MyBean();
-    	pojo.setName("waylau.com");
-    	pojo.setAge(28);
-        return pojo;
-    }
-    
-    @DELETE
-    @Path("pojojson")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String deletePojoJson(@QueryParam("name") String name ) {
-        return  "You delete " + name;
-    }
-    
-    @DELETE
-    @Path("pojojson/{name}")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String deletePojoJsonPath(@PathParam("name") String name ) {
-        return  "You delete " + name;
-    }
-    
-    //下面的例子是无法接收到 body 作为参数的反面例子
-//    @DELETE
-//    @Path("pojojson")
-//    @Consumes(MediaType.APPLICATION_JSON)
-//    public String deletePojoJsonBody(@QueryParam("name") String name ) {
-//        return  "You delete " + name;
-//    }
- 
+	 *
+	 * @return MyPojo 以 application/json 形式响应
+	 */
+	@GET
+	@Path("pojojson")
+	@Produces(MediaType.APPLICATION_JSON)
+	public MyBean getPojoJson() {
+		MyBean pojo = new MyBean();
+		pojo.setName("waylau.com");
+		pojo.setAge(28);
+		return pojo;
+	}
+
+	@DELETE
+	@Path("pojojson")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deletePojoJson(@QueryParam("name") String name) {
+		return "You delete " + name;
+	}
+
+	@DELETE
+	@Path("pojojson/{name}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deletePojoJsonPath(@PathParam("name") String name) {
+		return "You delete " + name;
+	}
+
+	//下面的例子是无法接收到 body 作为参数的反面例子
+	//    @DELETE
+	//    @Path("pojojson")
+	//    @Consumes(MediaType.APPLICATION_JSON)
+	//    public String deletePojoJsonBody(@QueryParam("name") String name ) {
+	//        return  "You delete " + name;
+	//    }
+
 }
